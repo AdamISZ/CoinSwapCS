57185f7d43f16a8b1767010df2091446896c758b
fivepiece <fivepiece@users.noreply.github.com>
2017-05-02 19:44:35 +0300

    - make `rm -rf` less scary - run tests in RAM - add option to keep test files - add ability to handle stale bitcoind daemons

--- test/run_all.sh
+++ test/run_all.sh
@@ -1,13 +1,98 @@
-x=( "cooperative" "badhandshake" "fakesecret" \
-"badhandshake" "badncs" "badcompleten" "badsendtx0id" "badreceivetx1id" \
-"badsendtx3sig" "nobroadcasttx0" "notx01monitor" "badreceivetx5sig" \
-"cbadhandshake" "cbadnegotiate" "cbadsendtx1id" "cbadreceivetx3sig" \
-"cnobroadcasttx1" "cbadreceivesecret" "cbadsendtx5sig" "cbadreceivetx4sig")
- 
-for thing in ${x[@]}; do
-    py.test --btcroot=$1 --btcpwd=123456abcdef --btcconf=$2 --runtype="${thing}"
-    [[ $? != 0 ]] && break && echo 1
-    rm -rf ~/.bitcoin/regtest
-    sleep 2
-done && echo 0
+#!/bin/bash
 
+mk_bitcoinconf()
+{
+    cp ./regtest_bitcoin.conf ${tmpdir}/bitcoin.conf
+    if [[ $? != 0 ]]; then
+        echo "regtest_bitcoin.conf not found. re-run this script from the ./test directory" 2>&1
+        exit 1
+    fi
+}
+
+mk_coinswapconf()
+{
+    cp ./regtest_coinswapcs.cfg ${tmpdir}/coinswapcs.cfg
+    if [[ ! -d $HOME/.CoinSwapCS ]]; then
+        mkdir $HOME/.CoinSwapCS
+    fi
+    sed -i 's/bitcoin_cli_cmd = bitcoin-cli/bitcoin_cli_cmd = '"${bitcoind_dir//\//\\\/}"'bitcoin-cli/' ${tmpdir}/coinswapcs.cfg
+}
+
+clean_datadir()
+{
+    if [[ "$1" =~ /dev/shm/cstest[0-9]* ]]; then
+        rm --preserve-root -rf "$1"
+    else
+        echo "not a temporary test directory : ${1}" 1>&2
+    fi
+
+    if [[ -d $1 ]]; then
+        echo "directory not removed : ${1}" 1>&2
+    fi
+}
+
+run_test()
+{
+    py.test --btcroot="${bitcoind_dir}" --btcpwd=123456abcdef --btcconf="$1" --runtype="$2" -s | tee ${curtest}/pytest.log
+    echo "$?"
+}
+
+main()
+{
+    local tests=( "cooperative" "badhandshake" "fakesecret" \
+        "badhandshake" "badncs" "badcompleten" "badsendtx0id" "badreceivetx1id" \
+        "badsendtx3sig" "nobroadcasttx0" "notx01monitor" "badreceivetx5sig" \
+        "cbadhandshake" "cbadnegotiate" "cbadsendtx1id" "cbadreceivetx3sig" \
+        "cnobroadcasttx1" "cbadreceivesecret" "cbadsendtx5sig" "cbadreceivetx4sig")
+
+    local bitcoind_="${1:-$(which bitcoind)}"
+    echo "using bitcoind : ${bitcoind_}" 2>&1
+    if [[ ! -x ${bitcoind_} ]]; then
+        echo "bitcoind not found or not executable : ${bitcoind_}" 1>&2
+        return 1
+    else
+        local bitcoind_dir="$(dirname ${bitcoind_})/"
+        path_restore="$PATH"
+        export PATH="${bitcoind_dir}:$PATH"
+        shift
+    fi
+
+    local tmpdir="/dev/shm/cstest${RANDOM}"
+    mkdir ${tmpdir}
+    if [[ ! -d ${tmpdir} ]]; then
+        echo "tmpdir not found or /tmp not writable : ${tmpdir}" 1>&2
+        return 1
+    fi
+    if [[ "$1" == --keep ]]; then
+        local keep=1
+        shift
+    fi
+
+    mk_bitcoinconf "${tmpdir}"
+    mk_coinswapconf "${tmpdir}"
+    local test_queue=( ${1:-${tests[@]}} )
+    for test_case in ${test_queue[@]}; do
+        curtest="${tmpdir}/${test_case}_${RANDOM}"
+        mkdir ${curtest}
+        cp ${tmpdir}/bitcoin.conf ${curtest}/bitcoin.conf
+        echo "datadir=${curtest}" >> ${curtest}/bitcoin.conf
+        ln -fs ${tmpdir}/coinswapcs.cfg $HOME/.CoinSwapCS/coinswapcs.cfg
+        echo "testing : ${curtest}" 1>&2
+        run_test "${curtest}/bitcoin.conf" "${test_case}"
+        sleep 2
+        if [[ -e ${curtest}/bitcoind.pid ]]; then
+            mapfile btcpid <${curtest}/bitcoind.pid
+            kill -9 ${btcpid}
+            echo "bitcoind stalled and killed. pid : ${btcpid}" 1>&2
+            sleep 1
+        fi
+    done
+
+    if (( ${keep:-0} == 0 )); then
+        clean_datadir "${tmpdir}"
+    fi
+    unlink $HOME/.CoinSwapCS/coinswapcs.cfg
+    export PATH="${path_restore}"
+}
+
+main $@
